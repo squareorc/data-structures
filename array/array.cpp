d3fae22596ef8b6019b52955467f5c0e2654dcd1
squareorc <harsh.bhogal7@gmail.com>
2025-03-21 21:02:27 +0530

    Initial Commit

---
+++ array/array.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    //declaring an array
+    /*
+    array is a collection of elements of the same datatype
+    placed in a contiguous memory locations [1000->1004->1008]
+    can be referenced using an index to a unique identifier
+    */
+    int arr[4] = { 10 , 20, 30, 40 };
+
+    cout << arr[0]<<arr[1]<<arr[2]<<arr[3];
+    return 0;
+
+}
